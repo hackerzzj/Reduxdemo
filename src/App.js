eada3686f2655fab4c5ce82988bbf66f4a856524
zuozhijie <zuozhijie@shineyue.com>
2021-09-29 16:31:09 +0800

    route初步尝试

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Index from './page';
+import List from './page/list';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './page/home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function AppRouter(){
+    return <Router>
+        <ul>
+          <li><Link to='/'>首页</Link></li>
+          <li><Link to='/list/123'>列表</Link></li>
+        </ul>
+        <Route path='/' exact component={Index}/>
+        <Route path='/list/:id'  component={List}/>
+        <Route path='/home/'  component={Home}/>
+    </Router>
 }
-
-export default App;
+export default AppRouter;
